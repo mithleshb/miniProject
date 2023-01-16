17c690be6eaeef393599083ce4e4b6d9c3b77701
Mithlesh Baghel <mithleshbaghel185@gmail.com>
2023-01-16 01:39:46 -0800

    added gitignore

---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import { useDispatch } from "react-redux";
+
+import { addNewTodo } from "../redux/actions";
+
+
+const TodoForm = () => {
+    const [text, setText] = useState("");
+
+    const dispatch = useDispatch();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(addNewTodo(text));
+        console.log("addnewtodo",text)
+
+        setText('');
+    }
+
+    const onInputChange = (e) => {
+        setText(e.target.value);
+        console.log("inputchange",e.target.value)
+    }
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input  
+                placeholder="Enter new todo..."
+                className="input"
+                onChange={onInputChange}
+                value={text}
+            />
+        </form>
+    )
+}
+
+export default TodoForm;
